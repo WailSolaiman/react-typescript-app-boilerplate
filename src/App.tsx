99b97aeee3b110b4b8001c3b47a22d7ff4ded439
Wail Solaiman <wailsolaiman@hotmail.com>
2021-11-01 14:05:06 +0100

    init boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Home from './views/home';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Typescript App</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import ButtonElement from "@mui/material/Button"
+
+const Button = () => {
+  return <ButtonElement variant="contained">Hello World</ButtonElement>
+}
+
+export default Button
